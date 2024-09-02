3c19993c96fa90dd677b876f9c28d7543e29c2d7
Kelvin Marcondes <kelvindesouza@hotmail.com>
2024-09-02 19:47:28 -0300

    implementado tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {
-        path: 'home', component: AppComponent,
-    }
+    { path: 'home', component: HomePageComponent },
 ];
