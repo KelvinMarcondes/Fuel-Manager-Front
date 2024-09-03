5482d8a9e8c96f1f4c7d44f86c448900fadc19ce
Kelvin Marcondes <kelvindesouza@hotmail.com>
2024-09-02 21:22:03 -0300

    implementado transição para cadastro e login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './home-page/login/login.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomePageComponent },
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { LoginComponent } from "../login/login.component";
+import { LoginComponent } from "./login/login.component";
+import { CommonModule } from '@angular/common';
+import { CadastroComponent } from "./cadastro/cadastro.component";
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [LoginComponent],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  imports: [LoginComponent, CommonModule, CadastroComponent],
+  templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
+
+  exibirLogin = true;
+
+  alternarLoginCadastro(): void {
+    this.exibirLogin = !this.exibirLogin;
+  }
+
+
 
 }
