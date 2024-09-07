e81213c8dc09bd7c7438eb933d7ba23990856683
Kelvin Marcondes <kelvindesouza@hotmail.com>
2024-09-06 22:57:00 -0300

    page notFound criada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     //paginas
@@ -8,5 +9,5 @@ export const routes: Routes = [
     
     // redirecionamento
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: '**', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', component: NotFoundComponent },
 ];
